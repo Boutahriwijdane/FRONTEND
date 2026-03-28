ac56d324f7a4ce3999a932713ca5ac0dcbc358eb
Wijdane Boutahri <boutahriwijdane@gmail.com>
2026-03-28 21:15:39 +0100

    tp4

--- tps/tp1/taskflow/src/main.tsx
+++ tps/tp1/taskflow/src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './features/auth/AuthContext'; 
 import './index.css'
 import App from './App.tsx'
+import 'bootstrap/dist/css/bootstrap.min.css'; 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- tps/tp1/taskflow/src/pages/Dashboard.tsx
+++ tps/tp1/taskflow/src/pages/Dashboard.tsx
@@ -2,12 +2,14 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 import { useAuth } from '../features/auth/AuthContext'
 import api from '../api/axios'
-import Header from '../components/Header'
+//import Header from '../components/Header'
 import Sidebar from '../components/Sidebar'
 import MainContent from '../components/MainContent'
 import ProjectForm from '../components/ProjectForm'
 import styles from './Dashboard.module.css'
 import ProjectCard from '../components/ProjectCard';
+//import HeaderMUI from '../components/HeaderMUI'
+import HeaderBS from '../components/HeaderBS'
 
 interface Project { id: string; name: string; color: string }
 interface Column { id: string; title: string; tasks: string[] }
@@ -122,7 +124,7 @@ export default function Dashboard() {
   return (
     <div className={styles.layout}>
 
-      <Header
+      <HeaderBS
         title="TaskFlow"
         onMenuClick={() => setSidebarOpen(p => !p)}
         userName={authState.user?.name}
